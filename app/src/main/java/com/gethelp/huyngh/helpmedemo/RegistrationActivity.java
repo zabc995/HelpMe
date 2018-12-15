e3c285a62eb8b27a473d2a544493d4b352bada2f
zabc995 <zabc995@gmail.com>
2018-12-16 00:37:08 +0700

    Declare new properties for Regis(DatabaseReference and Button)

--- app/src/main/java/com/gethelp/huyngh/helpmedemo/RegistrationActivity.java
+++ app/src/main/java/com/gethelp/huyngh/helpmedemo/RegistrationActivity.java
@@ -2,14 +2,20 @@ package com.gethelp.huyngh.helpmedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText edtFirstName, edtLastName, edtAddress, edtEmail;
     EditText edtPhoneNumber, edtPassword;
     RadioButton rdbGender;
+    Button btnSignUp;
+
+    DatabaseReference databaseAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
