db39514ead400975b74deaed25784f8be064277c
Son <ngocson.itlonely@gmail.com>
2018-12-16 13:41:02 +0700

    Complete screen RegistrationActivity And reupload logo picture

--- app/src/main/java/com/gethelp/huyngh/helpmedemo/LoginActivity.java
+++ app/src/main/java/com/gethelp/huyngh/helpmedemo/LoginActivity.java
@@ -2,12 +2,15 @@ package com.gethelp.huyngh.helpmedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Window;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/gethelp/huyngh/helpmedemo/RegistrationActivity.java
+++ app/src/main/java/com/gethelp/huyngh/helpmedemo/RegistrationActivity.java
@@ -1,10 +1,12 @@
 package com.gethelp.huyngh.helpmedemo;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+
 
 import com.google.firebase.database.DatabaseReference;
 
@@ -19,6 +21,9 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle("Registration");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_registration);
     }
 }
