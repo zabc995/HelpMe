f5ae958aa711bf4bc39045ec91052e220d4f3bc5
zabc995 <zabc995@gmail.com>
2018-12-18 15:30:03 +0700

    Test Please

--- app/src/main/java/com/gethelp/huyngh/helpmedemo/MainActivity.java
+++ app/src/main/java/com/gethelp/huyngh/helpmedemo/MainActivity.java
@@ -1,21 +1,24 @@
 package com.gethelp.huyngh.helpmedemo;
 
+import android.Manifest;
 import android.app.ActionBar;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import android.view.Window;
-
-import java.security.cert.Certificate;
 
 
 public class MainActivity extends BaseActivity {
@@ -24,29 +27,83 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        initView();
+        //
+        //
+        //initView();
+        //whereAmI();
+    }
+
+    private void whereAmI() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        Location lastLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (lastLocation != null)
+        {
+            LatLng latLng=new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(15)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            //Thêm MarketOption cho Map:
+            //MarkerOptions option=new MarkerOptions();
+            //option.title("Chỗ Tui đang ngồi đó");
+            //option.snippet("Gần làng SOS");
+            //option.position(latLng);
+            //Marker currentMarker= mGoogleMap.addMarker(option);
+            //currentMarker.showInfoWindow();
+        }
     }
 
     private void initView() {
         showDialogLoading();
-        SupportMapFragment supportMapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.frmMap);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.frmMap);
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(GoogleMap googleMap) {
-                mGoogleMap=googleMap;
+            public void onMapReady(final GoogleMap googleMap) {
+                mGoogleMap = googleMap;
                 mGoogleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
                     @Override
                     public void onMapLoaded() {
                         dismissDialog();
                         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+                        //
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        //
+                        mGoogleMap.setMyLocationEnabled(true);
                     }
                 });
-                    LatLng frmLastlocation = new LatLng(21.0166458, 105.7841248);
-                    mGoogleMap.addMarker(new MarkerOptions().position(frmLastlocation).title("I'm Here"));
-                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(frmLastlocation, 18));
+                whereAmI();
+                //frmLastlocation = new LatLng(21.0166458, 105.7841248);
+                //mGoogleMap.addMarker(new MarkerOptions().position(frmLastlocation).title("I'm Here"));
+                //mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(frmLastlocation, 18));
             }
         });
     }
