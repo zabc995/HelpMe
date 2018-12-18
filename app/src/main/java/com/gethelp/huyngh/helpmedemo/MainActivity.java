2dcea31ba23d7fa2987e29940550fdf67feb7193
Son <ngocson.itlonely@gmail.com>
2018-12-18 11:16:22 +0700

    Complete layout App Setting And reup icon in Main screen

--- app/src/main/java/com/gethelp/huyngh/helpmedemo/MainActivity.java
+++ app/src/main/java/com/gethelp/huyngh/helpmedemo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.gethelp.huyngh.helpmedemo;
 
 import android.app.ActionBar;
+import android.content.res.Configuration;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,6 +17,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.view.MenuItem;
 import android.view.Window;
 
 import java.security.cert.Certificate;
@@ -20,13 +26,39 @@ import java.security.cert.Certificate;
 
 public class MainActivity extends BaseActivity {
     private GoogleMap mGoogleMap;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.activity_main);
+        drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        
+
         initView();
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initView() {
